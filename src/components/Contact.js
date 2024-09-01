88589100eeb93305ee7de3d995b68b0c91600621
suresh kumar singh <121798736+10Suresh@users.noreply.github.com>
2024-09-01 23:58:30 +0530

    Frontend project
    first comment for frontend

---
+++ src/components/Contact.js
@@ -0,0 +1,94 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { toast ,Toaster} from 'react-hot-toast'; 
+
+function Contact() {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: ''
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      console.log(process.env.REACT_APP_BACKEND_URL, "djsdjd");
+      const response = await axios.post(
+        `${process.env.REACT_APP_BACKEND_URL}/api/send`,
+        formData
+      );
+      if (response.status === 200) {
+        toast.success('Message sent successfully');
+        setFormData({
+          name: '',
+          email: '',
+          message: ''
+        });
+      } else {
+        toast.error('Message could not be sent');
+      }
+    } catch (error) {
+      console.error('Error sending message:', error);
+      toast.error('Error sending message');
+    }
+  };
+
+  return (
+    <div className="contact-container">
+         <Toaster />
+      <div className='first-box'>
+        <img alt='Contact Us' src='https://www.searchenginejournal.com/wp-content/uploads/2022/08/contact-us-2-62fa2cc2edbaf-sej.png' />
+      </div>
+      <div className="form-wrapper">
+        <h1 className="form-title">Contact Form</h1>
+        <form onSubmit={handleSubmit} className="contact-form">
+          <div className="form-group">
+            <label htmlFor="name">Name:</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+              required
+              className="form-input"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+              className="form-input"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="message">Message:</label>
+            <textarea
+              id="message"
+              name="message"
+              value={formData.message}
+              onChange={handleChange}
+              required
+              className="form-input"
+            />
+          </div>
+          <button type="submit" className="submit-button">Submit</button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Contact;
